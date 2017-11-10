f29be26f88207705fb4c6945621f2879273a00db
Den <stereo069@gmail.com>
2017-11-10 21:43:41 +0300

    Design

--- html/js/main.js
+++ html/js/main.js
@@ -9,19 +9,11 @@ $(document).ready(function() {
     if(isHide){
       $('.frequence').css('margin-top','50vh');
       $('#frequenceArrow').css('transform','rotate(0deg)');
-      $('#frequenceArrow').css('color','white');
-      $('.frequence').css('background-color','#33abe5');
-      $('.hideFrequence').css('opacity','0.5');
-      $('.frequence').css('opacity','1');
       isHide = false;
     }
     else{
       $('.frequence').css('margin-top','94vh');
       $('#frequenceArrow').css('transform','rotate(180deg)');
-      $('.frequence').css('background-color','#33abe5');
-      $('#frequenceArrow').css('color','black');
-      $('.hideFrequence').css('opacity','0.5');
-      $('.frequence').css('opacity','0.5');
       isHide = true;
     }
   });
@@ -29,12 +21,10 @@ $(document).ready(function() {
     if(isEncode){
       $('.encoderBlock').css('top','0vh');
       $('#encoderArrow').css('transform','rotate(180deg)');
-      $(this).css('opacity','1');
       isEncode = false;
     }else{
       $('.encoderBlock').css('top','-44vh');
       $('#encoderArrow').css('transform','rotate(0deg)');
-      $(this).css('opacity','0.5');
       isEncode = true;
     }
   });
